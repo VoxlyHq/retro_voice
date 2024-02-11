4d957c95860f58b54a466759a47d119cbf28a78b
Matthew Campbell <hyper@hyperworks.nu>
2024-02-12 02:12:04 +0700

    testing e11 labs

--- eleven_labs.rb
+++ eleven_labs.rb
@@ -8,12 +8,19 @@ end
 
 ELEVEN_API_KEY=ENV["ELEVEN_API_KEY"]
 
-client = ElevenLabs::Client.new(api_key: ELEVEN_API_KEY)
+@e11_client = ElevenLabs::Client.new(api_key: ELEVEN_API_KEY)
 #voices = client.get('voices')
 #voice_id = voices['voices'].first['voice_id']
 
 
 def convert_eleven(voicename, content)
-    speech = client.post("text-to-speech/#{voicename}", { text: 'your text' }) # returns a binary string
-    puts speech
+    speech = @e11_client.post("text-to-speech/#{voicename}", { text: content  }) # returns a binary string
+
+    # Open a file in binary write mode
+    File.open('weee.mp3', 'wb') do |file|
+     file.write(speech)
+    end
 end
+
+
+convert_eleven("N2lVS1w4EtoT3dr4eOWO", "Ah, the young lord returns triumphant! You did secure Mysidia's Crystal, did you not?")
