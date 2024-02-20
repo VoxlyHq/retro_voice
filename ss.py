f5c3fa3cd0da2e9b7fe71a577538507b4bb3bb37
Matthew Campbell <hyper@hyperworks.nu>
2024-02-20 21:58:38 +0700

    diff images before running ocr

--- ss.py
+++ ss.py
@@ -7,11 +7,16 @@ import easyocr
 
 from thefuzz import fuzz
 from cloudvision import detect_text_google
+from image_diff import calculate_image_difference
 
+from PIL import Image
 
 # Detect the operating system
 os_name = platform.system()
 reader = easyocr.Reader(['en']) #(['ja', 'en']) 
+
+
+previous_image = Image.new('RGB', (100, 100), (255, 255, 255))
 
 if os_name == 'Windows':
     # Import Windows-specific module
@@ -37,43 +42,60 @@ def thefuzz_test(ocr_text):
 
     print(score)  # This will print the similarity score as a percentage
 
+def run_ocr(image):
+        # Use Tesseract to do OCR on the image
+#        text = pytesseract.image_to_string(img)
+    import time
+
+    start_time = time.time() # Record the start time
+    text = pytesseract.image_to_string('window_capture.jpg') #, lang='jpn')
+    print("found text terrasect----")
+    print(text)
+    print("----")
+    end_time = time.time() # Record the end time
+    print(f"Time taken: {end_time - start_time} seconds")
+    thefuzz_test(text)
+
+    start_time = time.time() # Record the start time
+    result = reader.readtext('window_capture.jpg')
+    print("found text easyocr----")
+    print(result)
+    print("----")
+    end_time = time.time() # Record the end time
+    print(f"Time taken: {end_time - start_time} seconds")
+
+    start_time = time.time() # Record the start time
+    # Replace the path below with the path to your image file
+    result = detect_text_google('window_capture.jpg')
+    print("found text google----")
+    print(result)
+    print("----")
+    print(f"Time taken: {end_time - start_time} seconds")
 
 def timed_action():
     print("Action triggered by timer")
+    global previous_image
     window_name = "RetroArch"  # Adjust this to the target window's name
     file_path = os.path.expanduser("window_capture.jpg")  # Save location
     window_id = find_window_id(window_name)
     if window_id:
         img = capture_window_to_file(window_id, file_path)
 
-        # Use Tesseract to do OCR on the image
-#        text = pytesseract.image_to_string(img)
-        import time
 
-        start_time = time.time() # Record the start time
-        text = pytesseract.image_to_string('window_capture.jpg') #, lang='jpn')
-        print("found text terrasect----")
-        print(text)
-        print("----")
-        end_time = time.time() # Record the end time
-        print(f"Time taken: {end_time - start_time} seconds")
-        thefuzz_test(text)
+        print("previous_image--{previous_image}")
+        print(previous_image)
+        print("-0--")
+        percent_diff = calculate_image_difference(img, previous_image)
+        print(f'Images differ by {percent_diff:.2f}%')
 
-        start_time = time.time() # Record the start time
-        result = reader.readtext('window_capture.jpg')
-        print("found text easyocr----")
-        print(result)
-        print("----")
-        end_time = time.time() # Record the end time
-        print(f"Time taken: {end_time - start_time} seconds")
-
-        start_time = time.time() # Record the start time
-        # Replace the path below with the path to your image file
-        result = detect_text_google('window_capture.jpg')
-        print("found text google----")
-        print(result)
-        print("----")
-        print(f"Time taken: {end_time - start_time} seconds")
+        # Decide whether to call OCR based on the difference
+        if percent_diff > 10:
+            print("Images are more than 10% different. Proceed with OCR.")
+            run_ocr(img)
+            previous_image = img
+        else:
+            print("Difference is less than 10%. No need to call OCR again.")
+            
 
     else:
         print(f"No window found with name containing '{window_name}'.")
