1b8618d80c76df516622fdd1ce0ffd18a3ddabd3
ShaneKoNaung <shanekonaung@gmail.com>
2024-05-10 16:26:31 +0700

    disable dialog, also disable audio voiceover

--- ss.py
+++ ss.py
@@ -24,7 +24,6 @@ os_name = platform.system()
 dialogues = {}
 
 
-frameProcessor =  FrameProcessor()
 last_played = -1
 show_image_screen = False 
 video_stream = None
@@ -149,11 +148,12 @@ def process_screenshot(img,translate=None, show_image_screen=False, enable_cache
     closest_match, previous_image, highlighted_image, annotations, translation = frameProcessor.run_image(img, translate=translate,enable_cache=enable_cache)
 
     if closest_match != None and closest_match != last_played:
-        start_time = time.time() # Record the start time
-        formated_filenames = [format_filename(i) for i in closest_match]
-        play_audio_threaded(formated_filenames)
-        end_time = time.time()
-        print(f"Audio Time taken: {end_time - start_time} seconds")
+        if not frameProcessor.disable_dialog:
+            start_time = time.time() # Record the start time
+            formated_filenames = [format_filename(i) for i in closest_match]
+            play_audio_threaded(formated_filenames)
+            end_time = time.time()
+            print(f"Audio Time taken: {end_time - start_time} seconds")
         last_played = closest_match
         if show_image_screen:
             set_annotation_text(annotations)
@@ -223,14 +223,20 @@ def main():
     parser.add_argument('-fps', '--show_fps',  action='store_true', help="Show fps")
     parser.add_argument('-trans', '--translate', type=str, help="Translate from source language to target language eg. en,jp")
     parser.add_argument('-c', '--enable_cache', action='store_true', help="Enable cache")
+    parser.add_argument('-dd', '--disable_dialog', action='store_true', help="disable dialog")
 
     
 
     args = parser.parse_args()
+    disable_dialog = args.disable_dialog
     if args.japanese:
         set_dialog_file("static/dialogues_jp_web.json")
         lang = "jp"
-        frameProcessor =  FrameProcessor(lang) 
+        
+        frameProcessor =  FrameProcessor(lang, disable_dialog) 
+    
+    if not args.japanese:
+        frameProcessor = FrameProcessor(disable_dialog=disable_dialog)
 
     if args.webserver:
         server_thread = threading.Thread(target=run_server)
