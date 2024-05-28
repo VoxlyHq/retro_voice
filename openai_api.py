2dfd63c06d9ec81263a4aeddf61ae71eb31406d4
ShaneKoNaung <shanekonaung@gmail.com>
2024-05-28 19:31:37 +0700

    Updated translation to gpt4o

--- openai_api.py
+++ openai_api.py
@@ -9,7 +9,7 @@ OpenAI_API_KEY = os.environ.get("OPENAI_ACCESS_TOKEN")
 
 class OpenAI_API:
     
-    def __init__(self, translation_model="gpt-3.5-turbo", 
+    def __init__(self, translation_model="gpt-4o", 
                        vision_model="gpt-4o"):
         self.translation_model = translation_model
         self.vision_model = vision_model
