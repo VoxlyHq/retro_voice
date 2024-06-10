850c02efef2d672cbe2c5f0ab51769c1dcca06bc
ShaneKoNaung <shanekonaung@gmail.com>
2024-06-10 08:21:38 +0700

    Added Eval Mode for UserVideo

--- user_video.py
+++ user_video.py
@@ -8,16 +8,37 @@ dummy_image = Image.new('RGB', (100, 100), (255, 255, 255))
 
 #This handles per user video processing
 class UserVideo:
-    def __init__(self, lang="jp", disable_dialog=False, disable_translation=False, enable_cache=False, translate="", textDetector=None, debug_bbox=False):
+    def __init__(self, lang="jp", disable_dialog=False, disable_translation=False, enable_cache=False, translate="", textDetector=None, debug_bbox=False, eval=False):
         self.last_inboard_frame = None
         self.last_frame_count = 0
         self.crop_height = 71
 
-        self.frameProcessor = FrameProcessor(lang, disable_dialog,)
+        if eval is False:
+            self.frameProcessor = FrameProcessor(lang, disable_dialog)
 
-        self.video_stream = VideoStreamWithAnnotations(background_task=self.process_video_thread, background_task_args={"translate" : translate, 'enable_cache' : enable_cache},
-                                                    show_fps=True, crop_y_coordinate=None, frameProcessor=self.frameProcessor, textDetector=textDetector, debug_bbox=debug_bbox) #TODO crop should be set later by user
-        #video_stream.stop()
+            self.video_stream = VideoStreamWithAnnotations(
+                background_task=self.process_video_thread,
+                background_task_args={"translate": translate, 'enable_cache': enable_cache},
+                show_fps=True,
+                crop_y_coordinate=None,
+                frameProcessor=self.frameProcessor,
+                textDetector=textDetector,
+                debug_bbox=debug_bbox
+            )
+
+        if eval is True:
+            self.frameProcessor_jp = FrameProcessor(language='jp', disable_dialog=disable_dialog)
+            self.frameProcessor_en = FrameProcessor(language='en', disable_dialog=disable_dialog)
+
+            self.video_stream = VideoStreamWithAnnotations(
+                background_task=self.process_video_thread,
+                background_task_args={"translate": translate, 'enable_cache': enable_cache},
+                show_fps=True,
+                crop_y_coordinate=None,
+                frameProcessor=self.frameProcessor_en,
+                textDetector=textDetector,
+                debug_bbox=debug_bbox
+            )
 
 
     def process_video_thread(self, translate="", enable_cache=False):
@@ -54,5 +75,39 @@ class UserVideo:
             
         print(f"processed_latest_inboard_frame {processed_latest_inboard_frame}")
         return processed_latest_inboard_frame
+    
+    def get_predictions(self, image_path):
+        """
+        Return the results of text detection, text recognition and translation for the Input Image.
+        """
+        results = []
+
+        print(image_path)
+        game, lang, number = image_path.stem.split('_')
+        img = Image.open(image_path)
+        img = self.video_stream.preprocess_image(img, crop_y_coordinate=self.crop_height)
+        
+        image = self.video_stream.textDetector.preprocess_image(image=img)
+        if not self.video_stream.textDetector.has_text(image):
+            return {
+                "image": str(image_path),
+                "text_detected": False,
+                "recognized_text": '',
+                "annotations": [],
+                "translation": ''
+            }
+        
+        if lang == 'EN':
+            closest_match, previous_image, highlighted_image, annotations, translation = self.frameProcessor_en.run_image(img, translate=self.video_stream.background_task_args.get("translate", None), enable_cache=self.video_stream.background_task_args.get("enable_cache", False))
+        if lang == 'JP':
+            closest_match, previous_image, highlighted_image, annotations, translation = self.frameProcessor_jp.run_image(img, translate=self.video_stream.background_task_args.get("translate", None), enable_cache=self.video_stream.background_task_args.get("enable_cache", False))
+
+        return {
+            "image": str(image_path),
+            "text_detected": True,
+            "recognized_text": closest_match,
+            "annotations": annotations,
+            "translation": translation
+        }
     
     
